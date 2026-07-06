b8fac962f77aa665c47e58204707da40de56d9a6
unknown <rtiappealtrackerproject@gmail.com>
2026-07-06 07:59:37 +0530

    Update metadata and seo keywords for better ranking

--- src/app/(tool)/compress/layout.js
+++ src/app/(tool)/compress/layout.js
@@ -1,13 +1,15 @@
 export const metadata = {
   title: "Free Video Compressor Online - 100% Private & Fast | Useful Tools Zone",
 description: 'Compress videos for WhatsApp, Gmail, or any custom size! Fast, 100% private, browser-based compression—your files never leave your device.',
-  keywords: [
-    "video compressor",
-    "compress video online",
-    "reduce video size",
-    "local video compressor",
+ keywords: [
+    "video compressor for whatsapp",
+    "compress video for gmail",
+    "no upload video compressor",
     "free video size reducer",
-    "data saver video compressor"
+    "private video resizer",
+    "compress mp4 online free",
+    "fast video compression browser",
+    "local video compressor"
   ],
   alternates: {
     canonical: "https://usefultoolszone.com/", // जब आप डोमेन लाइव करें, तो इसे अपडेट कर सकते हैं
